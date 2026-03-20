5356de938ce77edf20ad7aa86211781e0dd16544
Aruna <arunakaragulova123@gmail.com>
2026-03-20 20:19:12 +0500

    changes in task2

--- task2/second.go
+++ task2/second.go
@@ -25,7 +25,7 @@ func withMutex() {
 }
 
 func withAtomic() {
-	var counter atomic.Int64 
+	var counter atomic.Int64
 	var wg sync.WaitGroup
 
 	for i := 0; i < 1000; i++ {
@@ -43,3 +43,30 @@ func main() {
 	withMutex()
 	withAtomic()
 }
+
+
+
+/*
+The commented code below shows a data race problem.
+Its when multiple goroutines perform unsynchronized operations (read, write and modify)
+on a shared variable, and then it can lead to lost updates and inconsistent results.
+The operations on counter are not atomic and can be interrupted by other goroutines,
+resulting in a final value which is less than 1000
+
+func main() {
+	var counter int
+	var wg sync.WaitGroup
+
+	for i := 0; i < 1000; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			counter++
+		}()
+	}
+
+	wg.Wait()
+
+	fmt.Printf("Final counter: %d\n", counter)
+}
+*/
